9fee6db534b98ca8dc4f13b9de1878ba1b6bc1b8
savas87 <savas-erbas@live.de>
2020-04-06 15:17:37 +0200

    create a simple random echo

---
+++ 01.Basic_Syntax/random.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * simple random numbers
+ */
+echo rand(1,10) // echo an random number from 1 between 10 
+?>
---
+++ 01.Basic_Syntax/statement.php
@@ -0,0 +1,11 @@
+<?php
+
+// basic statement
+/**
+ * echo -> shows value, content etc.
+ * echo -> can include html tags
+ * statement ends with ;
+ * each statement is an instruction that must be followed by the webserver 
+ * before it can proceed to next instruction
+ */
+echo 'This is a <strong>test</strong>';
